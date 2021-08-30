37bd01c0f7cd6222b4ab1ba7ac6a60d7b2a00e6e
leonardobarone <leonardobarone26@gmail.com>
2021-08-30 14:05:12 +0200

    avanzamento layout

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
+// Il recap dei dati e l'output del prezzo finale va stampato in pagina (formattato con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+// Buon lavoro a domani!
